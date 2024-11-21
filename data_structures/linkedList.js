4490e2e37ed72901bbc030b1650b7db295b84451
dervisonurgurbuz <onurgurbuz97@gmail.com>
2024-11-21 16:59:37 +0100

    finished linked list

--- data_structures/linkedList.js
+++ data_structures/linkedList.js
@@ -16,13 +16,28 @@ class LinkedList {
             value: value,
             next: null
         };
+        // this states the initial linkedlist that where this method has been used.
        this.tail.next = newNode
        this.tail = newNode
        this.length += 1
        return this
     }
+
+
+    prepend (value){
+        const newNode = {
+            value: value,
+            next: null
+        };
+        newNode.next = this.head
+        this.head = newNode
+        this.length++;
+        return this
+    }
 }
 
 const myLinkedList = new LinkedList(19)
-myLinkedList.append(11)
-// console.log(myLinkedList)
+console.log(myLinkedList.append(11))
+console.log(myLinkedList.append(11123))
+console.log(myLinkedList.prepend(99))
+ 
