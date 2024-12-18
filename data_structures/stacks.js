cc9d8928fc9f815500e5295dce41f01269d5d440
dervisonurgurbuz <onurgurbuz97@gmail.com>
2024-12-18 13:38:12 +0300

    revised stacks

--- data_structures/stacks.js
+++ data_structures/stacks.js
@@ -8,36 +8,28 @@ class Stack {
         this.items = [];
     }
 
-    // Functions to be implemented
-    // push function
     push(element)
     {
-        // push element into the items
         this.items.push(element);
     }
-    // pop function
+
     pop()
     {
-        // return top most element in the stack
-        // and removes it from the stack
-        // Underflow if stack is empty
         if (this.items.length == 0)
             return 'Underflow'
         return this.items.pop();
     }
+
     peek()
     {
-        // return the top most element from the stack
-        // but does'nt delete it.
         return this.items[this.items.length - 1];
     }
-   // isEmpty function
+   
     isEmpty()
     {
-        // return true if stack is empty
         return this.items.length == 0;
     }
-    // printStack function
+   
     printStack()
     {
         let str = "";
@@ -48,20 +40,15 @@ class Stack {
 }
 
 let stack = new Stack
-// Adding element to the stack
+
 stack.push(10);
 stack.push(20);
 stack.push(30);
 
-// Printing the stack element
-// prints [10, 20, 30]
 console.log(stack.printStack());
 
-// returns 30
 console.log(stack.peek());
 
-// returns 30 and remove it from stack
 console.log(stack.pop());
 
-// returns [10, 20]
 console.log(stack.printStack());
